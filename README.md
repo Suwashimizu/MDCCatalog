4d51c35a394a3b05da895802a7eba33b9242cb47
suwashimizu <suwawa2013@gmail.com>
2019-04-13 13:07:06 +0900

    add first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# What is
+
+StoryBoardのようにUI一覧を表示する
+
+# Deference
+
+MaterialButton
+https://developer.android.com/reference/com/google/android/material/button/MaterialButton
+
---
+++ app/src/main/java/org/suwashizmu/widgetshowcase/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.suwashizmu.widgetshowcase
+
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
